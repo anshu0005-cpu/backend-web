37f72a8d8d10c2da5ad1c660571bbd6b0ee4bcb7
anshu0005-cpu <panwaranshu894@gmail.com>
2026-09-23 17:18:40 +0530

    Delete middleware directory

--- middleware/logger.js
+++
@@ -1,7 +0,0 @@
-function logger(req,res,next){
- const start=Date.now();
- res.on("finish",()=>console.log(`${new Date().toISOString()} | ${req.method} ${req.originalUrl} | ${res.statusCode} | ${Date.now()-start}ms`));
- next();
-}
-
-module.exports=logger;
